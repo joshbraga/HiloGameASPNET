00645952761be415f9ac3933821165a55f21680a
SKarsab <62398122+SKarsab@users.noreply.github.com>
2020-11-21 15:39:00 -0500

    GameWon potentially done

--- HiloGameASPNET/gameWon.aspx.cs
+++ HiloGameASPNET/gameWon.aspx.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Reflection;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace HiloGameASPNET
 {
-    public partial class gameWon : System.Web.UI.Page
+    public partial class gameWon : System.Web.UI.Page`
     {
+        private string playerName;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (PreviousPage != null && PreviousPageViewState != null)
+            {
+                playerName = (string)PreviousPageViewState[SharedValues.viewStatePlayerName];
+                ViewState[SharedValues.viewStatePlayerName] = playerName;
+                ViewState[SharedValues.viewStateMaxGuess] = "";
+                ViewState[SharedValues.viewStateMinGuess] = "";
+                ViewState[SharedValues.viewStatePlayerGuess] = "";
+            }
+        }
 
+        private StateBag PreviousPageViewState
+        {
+            get
+            {
+                StateBag returnValue = null;
+                if (Page.PreviousPage != null)
+                {
+                    Object objPreviousPage = (Object)PreviousPage;
+                    MethodInfo objMethod = objPreviousPage.GetType().GetMethod("ReturnViewState");      //System.Reflection class
+                    return (StateBag)objMethod.Invoke(objPreviousPage, null);
+                }
+                return returnValue;
+            }
+        }
+
+        public StateBag ReturnViewState()
+        {
+            return ViewState;
         }
 
         protected void playAgainOnClick(object sender, EventArgs e)
